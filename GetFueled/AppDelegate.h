154c73f93d7d75be208e3ab5d4d375b76dd85403
Leonty Deriglazov <leontyd@me.com>
2014-08-27 20:32:02 +0700

    Initial Commit

---
+++ GetFueled/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  GetFueled
+//
+//  Created by Leonty Deriglazov on 27/08/2014.
+//  Copyright (c) 2014 Leonty Deriglazov. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ GetFueled/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  GetFueled
+//
+//  Created by Leonty Deriglazov on 27/08/2014.
+//  Copyright (c) 2014 Leonty Deriglazov. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
